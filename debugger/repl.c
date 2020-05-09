9ad3727c6324cc5a4875e9f6aa9d25069c054504
André Wagner <andre.nho@gmail.com>
2020-05-09 16:21:48 +0100

    .

--- debugger/repl.c
+++ debugger/repl.c
@@ -1,11 +1,17 @@
 #include "repl.h"
 
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "io.h"
 #include "serial.h"
 
 static const char* H_HELP = "h: help\r\n";
 static const char* W_HELP = "w ADDR DATA: write byte to memory\r\n";
 static const char* R_HELP = "r ADDR: read byte from memory\r\n";
+static const char* U_HELP = "u SIZE: enter programming mode\r\n";
+
+static int prog_size = 0;
 
 static void
 print_help()
@@ -13,6 +19,7 @@ print_help()
     ser_printstr(H_HELP);
     ser_printstr(W_HELP);
     ser_printstr(R_HELP);
+    ser_printstr(U_HELP);
     ser_printstr("data format is uppercase hexa (ex. 0C AF 12)\r\n");
 }
 
@@ -21,27 +28,41 @@ repl_do()
 {
     int pars;
     unsigned data1, data2;
-    ser_printstr("? ");
-    switch (ser_input(&data1, &data2, &pars)) {
-        case 'h':
-            print_help();
-            return;
-        case 'w':
-            if (pars == 2)
-                io_write(data1, data2);
-            else
-                ser_printstr(W_HELP);
-            return;
-        case 'r':
-            if (pars == 1)
-                ser_printhex(io_read(data1), 2);
-            else
-                ser_printstr(R_HELP);
-            return;
-        case 0:
-            return;
+    if (prog_size == 0) {
+        ser_printstr("? ");
+        switch (ser_input(&data1, &data2, &pars)) {
+            case 'h':
+                print_help();
+                return;
+            case 'w':
+                if (pars == 2)
+                    io_write(data1, data2);
+                else
+                    ser_printstr(W_HELP);
+                return;
+            case 'r':
+                if (pars == 1)
+                    ser_printhex(io_read(data1), 2);
+                else
+                    ser_printstr(R_HELP);
+                return;
+            case 'u':
+                if (pars == 1)
+                    prog_size = data1;
+                else
+                    ser_printstr(U_HELP);
+                return;
+            case 0:
+                return;
+        }
+        ser_printstr("Syntax error.\r\n");
+    } else {
+        for (int i = 0; i < prog_size; ++i)
+            io_write(i, ser_inputhex());
+        for (int i = 0; i < prog_size; ++i)
+            ser_printhex(io_read(i), 2);
+        prog_size = 0;
     }
-    ser_printstr("Syntax error.\r\n");
 }
 
 // vim:ts=4:sts=4:sw=4:expandtab
--- debugger/serial.c
+++ debugger/serial.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ser_init()
+void
+ser_init()
 {
     // set baud rate
     int ubrr = 12;  // 38400 at 8 Mhz - http://ruemohr.org/~ircjunk/avr/baudcalc/avrbaudcalc-1.0.8.php?postbitrate=38400&postclock=8
@@ -17,37 +18,49 @@ void ser_init()
     UCSRB = (1<<RXEN) | (1<<TXEN);     // Enable Receiver and Transmitter
 }
 
-void ser_printchar(char c)
+void
+ser_printchar(char c)
 {
     while (!( UCSRA & (1<<UDRE))); // Wait for empty transmit buffer
     UDR = c;
 }
 
-void ser_printstr(const char* text)
+void
+ser_printstr(const char* text)
 {
     for (int i = 0; text[i]; ++i)
         ser_printchar(text[i]);
 }
 
-void ser_printhex(unsigned value, int digits)
+void
+ser_printhex(unsigned value, int digits)
 {
+    (void) digits;
+
     char out[10];
     snprintf(out, sizeof out, "%X\n\r", value);
     ser_printstr(out);
 }
 
-char ser_input(unsigned* data1, unsigned* data2, int* pars)
+static void
+input_buf(char* buf, size_t sz)
 {
-    char buf[24] = { 0 };
     unsigned i = 0;
     do {
         while (!( UCSRA & (1<<RXC)));  // wait for empty receive buffer
-        if (i < sizeof buf) {
+        if (i < sz) {
             buf[i++] = UDR;
             ser_printchar(buf[i-1]);
         }
     } while (buf[i-1] != '\r');
     ser_printchar('\n');
+}
+
+char
+ser_input(unsigned* data1, unsigned* data2, int* pars)
+{
+    char buf[24] = { 0 };
+    input_buf(buf, sizeof buf);
 
     char command = 0;
     int n = sscanf(buf, "%c %x %x", &command, data1, data2);  // TODO - do this manually if there's no space in uc
@@ -62,6 +75,16 @@ char ser_input(unsigned* data1, unsigned* data2, int* pars)
     */
     
     return command;
+}
+
+uint8_t
+ser_inputhex()
+{
+    char buf[24] = { 0 };
+    input_buf(buf, sizeof buf);
+    int data;
+    sscanf(buf, "%x", &data);
+    return (uint8_t) data;
 }
 
 // vim:ts=4:sts=4:sw=4:expandtab
