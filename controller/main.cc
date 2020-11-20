f2863d092bf97daf0a6961daa5ac6ca574fa0ab4
André Wagner <WagnerAndre@JohnDeere.com>
2020-11-20 17:50:43 +0000

    .

--- controller/main.cc
+++ controller/main.cc
@@ -1,4 +1,15 @@
+#include "io.hh"
+#include "serial.hh"
+#include "repl.hh"
+
 int main()
 {
+    IO     io;
+    Serial serial;
+    Repl   repl(serial, io);
 
+    while (1)
+        repl.execute();
 }
+
+// vim:ts=4:sts=4:sw=4:expandtab
