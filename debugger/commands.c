bcbee6c78e1fb2b74a31517e4d03de1a0a0926e5
André Wagner <WagnerAndre@JohnDeere.com>
2020-11-30 14:40:02 +0000

    .

--- debugger/commands.c
+++ debugger/commands.c
@@ -1,7 +1,34 @@
 #include "commands.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    const char*    command;
+    void         (*function)();
+    const char*    help;
+} Command;
+
+static void command_help()
+{
+
+}
+
+static Command commands[] = {
+    { "help", command_help, "help [FUNCTION]: show help for function" },
+};
+
 void command_do(const char* line)
 {
+    char cmd[21];
+    int n = sscanf(line, "%20s", cmd);
+    if (n == 1) {
+        for (size_t i = 0; i < sizeof commands / sizeof(Command); ++i) {
+            if (strncmp(cmd, commands[i].command, strlen(cmd)) == 0)
+                commands[i].function();
+        }
+    }
 }
 
 // vim:ts=4:sts=4:sw=4:expandtab
