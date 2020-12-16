211d5a5135c23dc48560bb85428627d708b1565b
André Wagner <WagnerAndre@JohnDeere.com>
2020-12-16 23:18:32 +0000

    .

--- firmware/run.c
+++ firmware/run.c
@@ -1,6 +1,8 @@
 #include "run.h"
 
 #include <stdbool.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
 #include <avr/io.h>
 
 #include "lowlevel.h"
@@ -11,27 +13,62 @@
 #define CLK_UP   0b00001011
 #define CLK_DOWN 0b00000011
 
+static uint8_t last_key_pressed;
+
 void run()
 {
     serial_putsstr(PSTR("Welcome to the Z80-AW computer.\r\n"));
 
-    z80_init();
-    bool last_iorq = true;
+    // setup RX interrupt
+    UCSRB |= (1 << RXEN) | (1 << RXCIE);
 
+    // setup INT0 interrupt (for IORQ - video)
+    GICR |= (1 << INT0);    // enable interrupt 0
+    MCUCR |= (1 << ISC01);   // on falling edge
+    MCUCR &= ~(1 << ISC00);
+    sei();
+
+    z80_init();
+
+    // TODO - replace by an AVR timer
     for (;;) {
         PORTB = CLK_UP;
         PORTB = CLK_DOWN;
+    }
+}
 
-        // check for video interrupt (TODO - move this to AVR interrupt)
-        if (last_iorq == 1 && get_IORQ() == 0) {
-            uint16_t addr = memory_read_addr();
-            if ((addr & 0xff) == 0x00) {   // video device
-                uint8_t data = addr >> 8;
-                serial_send(data);
-            }
-        }
+ISR(INT0_vect)
+{
+    uint16_t addr = memory_read_addr();
+    if ((addr & 0xff) == 0x00) {   // video device
+        uint8_t data = addr >> 8;
+        serial_send(data);
+    }
+}
 
-        last_iorq = get_IORQ();
+ISR(USART_RXC_vect)
+{
+    last_key_pressed = serial_recv();
+
+    // fire interrupt
+    set_INT(0);
+    set_BUSREQ(1);
+    for (int i = 0; i < 15; ++i) {
+        PORTB = CLK_UP;
+        PORTB = CLK_DOWN;
+        if (get_IORQ() == 0)
+            goto z80_response;
+    }
+    set_INT(1);   // a interrupt request was not accepted by Z80
+    return;
+
+z80_response:
+    set_INT(1);
+
+    for (int i = 0; i < 3; ++i) {
+        memory_set_data(0xcf);
+        PORTB = CLK_UP;
+        PORTB = CLK_DOWN;
     }
 }
 
