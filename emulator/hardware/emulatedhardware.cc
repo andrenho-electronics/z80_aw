d581153bc78cd00a2c33d744acc23a52b45aff4c
André Wagner <WagnerAndre@JohnDeere.com>
2020-12-23 14:58:01 -0300

    .

--- emulator/hardware/emulatedhardware.cc
+++ emulator/hardware/emulatedhardware.cc
@@ -1,7 +1,7 @@
 #include "emulatedhardware.hh"
 
 EmulatedHardware::EmulatedHardware()
-    : z80_({})
+    : z80_({}), memory_()
 {
     z80_.TrapBadOps = 1;
     z80_.User = this;
--- emulator/hardware/emulatedhardware.hh
+++ emulator/hardware/emulatedhardware.hh
@@ -9,12 +9,12 @@ class EmulatedHardware : public Hardware {
 public:
     EmulatedHardware();
 
-    void    set_memory(uint16_t addr, uint8_t data) override { memory[addr] = data; }
-    uint8_t get_memory(uint16_t addr) override { return memory[addr]; }
+    void    set_memory(uint16_t addr, uint8_t data) override { memory_[addr] = data; }
+    uint8_t get_memory(uint16_t addr) override { return memory_[addr]; }
 
 private:
     Z80 z80_;
-    uint32_t memory[64 * 1024];
+    uint32_t memory_[64 * 1024];
 };
 
 #endif //EMULATOR_EMULATEDHARDWARE_HH
