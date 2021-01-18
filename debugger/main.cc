f4617460bc37728a1f07475118dc752bb10a0591
André Wagner <WagnerAndre@JohnDeere.com>
2021-01-18 16:26:42 -0300

    .

--- debugger/main.cc
+++ debugger/main.cc
@@ -4,9 +4,12 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include "glad/include/glad/glad.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 void error_callback(int error, const char* description)
 {
+    (void) error;
     std::cerr << description << "\n";
 }
 
@@ -14,12 +17,14 @@ int main()
 {
     GLFWwindow* window;
     
+    glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         return EXIT_FAILURE;
-    glfwSetErrorCallback(error_callback);
     
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
     window = glfwCreateWindow(800, 600, "Z80AW Debugger", nullptr, nullptr);
     if (!window) {
@@ -28,16 +33,45 @@ int main()
     }
     
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     
-    glfwSwapInterval(1);
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
+    
+    bool show_demo_window = true;
+    
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
         glfwPollEvents();
+        
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+        ImGui::Render();
+        
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.3f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
+        glfwSwapBuffers(window);
     }
+    
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     
     glfwDestroyWindow(window);
     glfwTerminate();
+    
     return 0;
 }
---
+++ debugger/window.hh
@@ -0,0 +1,8 @@
+#ifndef DEBUGGER_WINDOW_HH
+#define DEBUGGER_WINDOW_HH
+
+class Window {
+
+};
+
+#endif
